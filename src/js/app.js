0bc0b640124b0072b2e5ff83d57d524851b4bdaa
Simon Nystrom <52610222+simstrom@users.noreply.github.com>
2021-11-19 09:03:28 +0100

    design: smooth transitions and loading spinner

--- src/js/app.js
+++ src/js/app.js
@@ -1,56 +1,42 @@
-
+const STD_DELAY = 500;
 const startBtn = document.querySelector('#start-btn');
-const startState = document.querySelector('#start')
+const startState = document.querySelector('#start');
 const quizContainer = document.querySelector('#question-box');
 const submitBtn = quizContainer.lastElementChild;
 const question = quizContainer.firstElementChild;
-const possibleAnswers = document.querySelectorAll('input[type="radio"]')
-const possibleAnswersText = document.querySelectorAll('label')
+const possibleAnswers = document.querySelectorAll('input[type="radio"]');
+const possibleAnswersText = document.querySelectorAll('label');
+const spinner = document.createElement('div');
 
 const quizData = [
     {
-        question : 'What is the abbreviation of Laughing out Loud?',
-        answers : [
-            'lol',
-            'lmfao',
-            'rofl',
-            'omg'
-        ],
+        question: 'What is the abbreviation of Laughing out Loud?',
+        answers: ['lol', 'lmfao', 'rofl', 'omg'],
         // a : 'Lol',
         // b: 'Lmfao',
         // c: 'Rofl',
         // d: 'Omg',
-        correct : 'a'
+        correct: 'a',
     },
     {
-        question : 'Abbreviation of Oh My God?',
-        answers : [
-            'lol',
-            'lmfao',
-            'rofl',
-            'omg'
-        ],
+        question: 'Abbreviation of Oh My God?',
+        answers: ['lol', 'lmfao', 'rofl', 'omg'],
         // a : 'Lol',
         // b: 'Lmfao',
         // c: 'Rofl',
         // d: 'Omg',
-        correct : 'd'
+        correct: 'd',
     },
     {
-        question : 'Abbreviation of Rofl?',
-        answers : [
-            'lol',
-            'lmfao',
-            'rofl',
-            'omg'
-        ],
+        question: 'Abbreviation of Rofl?',
+        answers: ['lol', 'lmfao', 'rofl', 'omg'],
         // a : 'Lol',
         // b: 'Lmfao',
         // c: 'Rofl',
         // d: 'Omg',
-        correct : 'c'
-    }
-]
+        correct: 'c',
+    },
+];
 let currentQuestion = 0;
 let score = 0;
 
@@ -59,7 +45,7 @@ const loadQuiz = (currentQuestion) => {
     for (let i = 0; i < possibleAnswersText.length; i++) {
         possibleAnswersText[i].innerText = quizData[currentQuestion].answers[i];
     }
-}
+};
 
 const getAnswer = () => {
     for (const answer of possibleAnswers) {
@@ -69,17 +55,30 @@ const getAnswer = () => {
         }
     }
     return null;
-}
+};
 
 const loadQuizState = () => {
-    quizContainer.style.display = 'flex';
-}
+    spinner.classList.add('lds-dual-ring');
+    document.body.prepend(spinner);
+    setTimeout(() => {
+        changeContentSmooth(quizContainer);
+        quizContainer.style.display = 'flex';
+    }, STD_DELAY);
+};
 
 const loadEndState = () => {
     quizContainer.innerHTML = `<h2>Your Score</h2>
     <h3>${score} / ${quizData.length}</h3>
-    <button onclick="location.reload()" class="primary-btn">Try Again</button>`
-}
+    <button onclick="location.reload()" class="primary-btn">Try Again</button>`;
+};
+
+const changeContentSmooth = async (content) => {
+    content.classList.add('fade');
+    setTimeout(() => {
+        content.classList.remove('fade');
+        spinner.style.display = 'none';
+    }, STD_DELAY);
+};
 
 submitBtn.addEventListener('click', () => {
     const answer = getAnswer();
@@ -91,18 +90,21 @@ submitBtn.addEventListener('click', () => {
             console.log(`Wrong! Your Score : ${score}`);
         }
         currentQuestion++;
+        changeContentSmooth(quizContainer);
         if (currentQuestion < quizData.length) {
-            loadQuiz(currentQuestion);
+            setTimeout(() => {
+                loadQuiz(currentQuestion);
+            }, STD_DELAY);
         } else {
-            loadEndState();
-            // alert('No more questions!')
-            // location.reload();
+            setTimeout(() => {
+                loadEndState();
+            }, STD_DELAY);
         }
     }
-})
+});
 
 startBtn.addEventListener('click', () => {
     startState.style.display = 'none';
     loadQuizState();
     loadQuiz(currentQuestion);
-})
+});
